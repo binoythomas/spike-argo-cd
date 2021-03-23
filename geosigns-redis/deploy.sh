7f13ad1bfbd010eedde4b4a04bbbf5195689e44d
Binoy Thomas <binoy.thomas@hitachivantara.com>
2021-03-23 09:07:25 -0500

    Support for Windows

--- geosigns-redis/deploy.sh
+++ geosigns-redis/deploy.sh
@@ -4,4 +4,9 @@ set -e
 REDIS_DIR=$1
 
 # substitute placeholders in patches
-sed -i '' "s|HOST_PATH|${HOME}/redis-data|g" "${REDIS_DIR}/set_host_path.yaml"
+machine_name="`uname -s | awk '{print $1}'`"
+if [ $machine_name != "Darwin" ]; then
+  sed -i "s|HOST_PATH|${HOME}/redis-data|g" "${REDIS_DIR}/set_host_path.yaml"
+else
+  sed -i '' "s|HOST_PATH|${HOME}/redis-data|g" "${REDIS_DIR}/set_host_path.yaml"
+fi
